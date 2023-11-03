0a911ab33305970c65707b1f39455e0bd1f49c91
juha44242 <104754622+juha44242@users.noreply.github.com>
2023-11-03 18:16:42 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,4 +1,17 @@
-<script> 
+<script>
+    // Get the current page's filename
+    var currentPage = location.pathname.split("/").slice(-1);
 
+    // Find the matching link and highlight it
+    var links = document.querySelectorAll(".tabnav");
+    for (var i = 0; i < links.length; i++) {
+        var link = links[i];
+        var linkHref = link.getAttribute("href");
 
+        // Check if the link's href matches the current page
+        if (linkHref === currentPage) {
+            link.classList.add("active"); // You can define a CSS class for highlighting the active link
+        }
+    }
+}
 </script>
